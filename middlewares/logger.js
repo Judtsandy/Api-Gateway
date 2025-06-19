f007835bbc75fb6d4580595249c22295eb5b38fd
SANDY JUDITH <91551677+Judtsandy@users.noreply.github.com>
2025-06-19 16:03:56 -0600

    Update logger.js

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,17 +1,24 @@
-// Funcion que representa un middleware
-const logger = (req, res, next)=>{
+// Funcion que representa un middleware mejorado
+const logger = (req, res, next) => {
     // Obtiene la fecha en tiempo real
-    const fecha = new Date().toDateString();
+    const fecha = new Date().toISOString();
     // Obtencion de todas las peticiones 
     const method = req.method;
     // Obtener los datos de la url
     const url = req.originalUrl;
-
-// Imprimir todos los datos
-console.log(`[${fecha}]${method} ${url}`);
-// Funcion para especificar que express llame al siguiente middleware
-next();
+    
+    // Log básico
+    console.log(`[${fecha}] ${method} ${url}`);
+    
+    // Log adicional para peticiones POST/PUT/PATCH
+    if (['POST', 'PUT', 'PATCH'].includes(method)) {
+        console.log(`[BODY] ${JSON.stringify(req.body)}`);
+        console.log(`[HEADERS] Content-Type: ${req.headers['content-type']}`);
+    }
+    
+    // Funcion para especificar que express llame al siguiente middleware
+    next();
 }
 
-// Exportaion del modulo
+// Exportacion del modulo
 module.exports = logger;
