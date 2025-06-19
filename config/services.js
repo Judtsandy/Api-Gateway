e4df1ffac66d6d9eeabcbb007eefb90320270bda
SANDY JUDITH <91551677+Judtsandy@users.noreply.github.com>
2025-06-18 20:21:33 -0600

    Update services.js

--- config/services.js
+++ config/services.js
@@ -3,25 +3,25 @@ const services = [
     {
         // API 1 - Usuarios
         name: "usuarios",
-        url: "https://api-users-citas-medicas-production.up.railway.app",
+        url: "https://api-users-citas-medicas-production.up.railway.app/api-1-user",
         path: "/api-usuarios"
     },
     {
         // API 2 - Auth
         name: "auth",
-        url: "https://api-2-auth-production.up.railway.app",
+        url: "https://api-2-auth-production.up.railway.app/api-2-auth",
         path: "/api-login"
     },
     {
         // API 3 - Services
         name: "services",
-        url: "https://api-3-services-production.up.railway.app",
+        url: "https://api-3-services-production.up.railway.app/api-3-services",
         path: "/api-services"
     },
     {
         // API 4 - Turnos
         name: "turnos",
-        url: "https://api-4-turnos-production-92f6.up.railway.app",
+        url: "https://api-4-turnos-production-92f6.up.railway.app/api-4-turnos",
         path: "/api-turnos"
     }
 ];
