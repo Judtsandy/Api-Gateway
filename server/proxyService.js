437cc5661fce7d81e6a2e4e33a25559c4c081968
SANDY JUDITH <91551677+Judtsandy@users.noreply.github.com>
2025-06-18 20:02:38 -0600

    Update proxyService.js

--- server/proxyService.js
+++ server/proxyService.js
@@ -1,22 +1,30 @@
-const {createProxyMiddleware, fixRequestBody} = require("http-proxy-middleware")
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-class ProxyService{
-    static createProxy(target){
+class ProxyService {
+    static createProxy(target) {
         return createProxyMiddleware({
-            target:target,
-            changeOrigin:true,
-            pathRewrite:(path, req) =>path.replace(req.baseUrl,""),
-            onProxyReq:(proxyReq, req)=>{
-                if(req.body && Object.keys(req.body).length){
+            target: target,
+            changeOrigin: true,
+            pathRewrite: (path, req) => path.replace(req.baseUrl, ""),
+            onProxyReq: (proxyReq, req) => {
+                if (req.body && Object.keys(req.body).length) {
                     const bodyData = JSON.stringify(req.body);
-
-                    proxyReq.setHeader('Content-Type', 'aplication/json');
+                    
+                    // Corregir el typo: 'application' en lugar de 'aplication'
+                    proxyReq.setHeader('Content-Type', 'application/json');
                     proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
                     proxyReq.write(bodyData);
                 }
+            },
+            onError: (err, req, res) => {
+                console.error('Proxy Error:', err);
+                res.status(500).json({
+                    error: 'Proxy Error',
+                    message: 'Unable to connect to target service'
+                });
             }
-        })
+        });
     }
 }
 
-module.exports=ProxyService;
+module.exports = ProxyService;
